bad4c868870efef6193984c0de2db47b5dc2661a
Moritz Pflanzer <moritz@pflanzer.eu>
2018-08-31 19:15:45 +0200

    Initial Commit

---
+++ CollectionView/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CollectionView
+//
+//  Created by Moritz Pflanzer on 31.08.18.
+//  Copyright © 2018 Moritz Pflanzer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CollectionView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by Moritz Pflanzer on 31.08.18.
+//  Copyright © 2018 Moritz Pflanzer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
